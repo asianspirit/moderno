07325afaac02c498ee676d29d8ae155e74a11b37
asianspirit <arslanshirazhiyev@gmail.com>
2020-04-08 17:48:08 +0600

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+      $('.product-slider__inner').slick({
+        dots:  true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      });
+
     var mixer = mixitup('.products__inner-box');
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -33,7 +34,8 @@ gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-         'node_modules/mixitup/dist/mixitup.js'
+         'node_modules/mixitup/dist/mixitup.js',
+         'node_modules/rateyo/src/jquery.rateyo.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())

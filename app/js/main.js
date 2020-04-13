2c71f5010cb7eb740e23dd4bc9c11b498b32f17e
asianspirit <arslanshirazhiyev@gmail.com>
2020-04-14 00:17:27 +0600

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,33 @@ $(function(){
         slidesToScroll: 4,
       });
 
+      $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+
+
+
     var mixer = mixitup('.products__inner-box');
 
 });
+
+
+$('.icon-th-list').on('click', function(){
+  $('.product__item').addClass('list');
+  $('.icon-th-list').addClass('active');
+  $('.icon-th-large').removeClass('active');
+  });
+
+  $('.icon-th-large').on('click', function(){
+    $('.product__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+    });
+    
--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,8 @@ gulp.task('style', function(){
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css',
-        'node_modules/rateyo/src/jquery.rateyo.css'
+        'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -35,7 +36,8 @@ gulp.task('script', function(){
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
          'node_modules/mixitup/dist/mixitup.js',
-         'node_modules/rateyo/src/jquery.rateyo.js'
+         'node_modules/rateyo/src/jquery.rateyo.js',
+         'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())

28d09c29b832829d76f13e5ad156d5ac39d3a0df
asianspirit <arslanshirazhiyev@gmail.com>
2020-04-20 21:31:48 +0600

    addded settings-page

--- app/js/main.js
+++ app/js/main.js
@@ -29,26 +29,28 @@ $(function(){
  
 
 $('.icon-th-list').on('click', function(){
-  $('.product__item').addClass('list');
+  $('product-page items.product__item').addClass('list');
   $('.icon-th-list').addClass('active');
   $('.icon-th-large').removeClass('active');
   });
 
   $('.icon-th-large').on('click', function(){
-    $('.product__item').removeClass('list');
+    $('product-page items.product__item').removeClass('list');
     $('.icon-th-large').addClass('active');
     $('.icon-th-list').removeClass('active');
     });
    
-    $('.product-one__tabs .tab').on('click', function(event) {
+  
+
+      
+    $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
       var id = $(this).attr('data-id');
-        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
         $(this).addClass('active');
         $('#'+id).addClass('active-tab').fadeIn();
         return false;
       });
-
 
 
 
